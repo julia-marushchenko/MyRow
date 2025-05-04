a27b57a7e88e4e09166c127019b47fdcefbec8bb
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-05-04 14:37:41 +0200

    Update Main.java
    Typo

--- src/com/rows/Main.java
+++ src/com/rows/Main.java
@@ -1,7 +1,7 @@
 package com.rows;
 /**
  * Java program to print rows like this
- *Input: n = 1, Output: 10 9 8 7 6 5 4 3 2 1
+ * Input: n = 1, Output: 10 9 8 7 6 5 4 3 2 1
  * Input n = 2, Output: 20 18 16 14 12 10 8 6 4 2
  * Condition 1 <= n<= 1000
  *
